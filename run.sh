9874923f505fdcd05e2bc803e34636ca2f685212
Yuantao Feng <yuantao.feng@outlook.com>
2025-07-21 18:03:31 +0800

    feat: get opencv_extra before running tests

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Get opencv_extra
+git clone https://github.com/opencv/opencv_extra
+cd opencv_extra
+git checkout 4.x
+cd ..
+
 # 4.x
 modules=("calib3d" "core" "dnn" "features2d" "imgcodecs" "imgproc" "objdetect" "photo" "stitching" "video" "videoio") # exclude "gapi"
 # 5.x
